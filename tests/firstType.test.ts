bab750420e07fd37903765d7cb33109ea5a4c508
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2025-06-13 16:26:41 +0100

    .

--- tests/firstType.test.ts
+++ tests/firstType.test.ts
@@ -1,4 +1,4 @@
-import { BaseModel, createModel, inMemoryStorageEngine } from "dist";
+import { createModel, inMemoryStorageEngine } from "dist";
 import { expect, test } from "bun:test";
 
 test("Defining the first type.", () => {
@@ -10,35 +10,38 @@ test("Defining the first type.", () => {
 
   const userModel = createModel(
     // The first parameter is a unique name for your type.
-    // This allows storage engines to avoid issues with overlapping IDs and
-    // possibly to optimize storage.
+    // This allows storage engines to avoid issues with overlapping IDs across
+    // different models, and possibly to optimize storage.
     "user",
-    // The second parameter shows TinyORM how to get a unique ID out of your
-    // type.
+    // The second parameter shows how to get a unique ID out of your type.
     // Annotating the type here is enough for typescript to infer it as the one
     // we'll be using for this model.
     (user: User) => user.username,
+    // We also specify a storage engine. TinyORM ships with a few, but we'll
+    // also see how you can write a custom one.
     inMemoryStorageEngine
   );
 
-  // You then simply create objects of your type.
-  // Your data is always a plain object, so you can just use it with libraries
-  // that expect that format - such as React's useState() hook.
+  // Your model works with regular objects of the type you specified.
+  // This means you can use them for anything that expects a plain object, such
+  // as React's useState() hook.
   const user: User = {
-    version: userModel.version, // Get current version number from the model.
     username: "hunter2",
     email: "hunter2@example.com",
   };
 
-  // To persist an object, use a method exposed by your selected storage engine:
+  // To persist an object, use a method exposed by your selected storage engine.
+  // inMemoryStorageEngine exposes .save():
   userModel.save(user);
 
-  // Same for retrieving it:
+  // Retrieving an object also relies on a method exposed by the storage engine.
+  // inMemoryStorageEngine exposes .get():
   const retrievedUser = userModel.get("hunter2");
 
   expect(retrievedUser.email).toBe("hunter2@example.com");
 
-  // We can also add helper methods to our models:
+  // We can also add helper methods to our models.
+  // Let's create a new model for our User type with a helper method:
   const userModel2 = createModel(
     "user",
     (user: User) => user.username,
@@ -51,7 +54,10 @@ test("Defining the first type.", () => {
     }
   );
 
-  // We can then call our utility method:
+  // We can then call our utility method.
+  // Models expose type hints correctly for both utility methods and the methods
+  // exposed by the specified storage engine, so your editor should be able to
+  // show you what's available.
   const domain = userModel2.getEmailDomain(user);
 
   expect(domain).toBe("example.com");
